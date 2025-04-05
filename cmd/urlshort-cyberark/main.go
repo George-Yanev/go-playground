efafcb1609e12ffdc120a833dbc017b2c00b5ec2
Georgi Yanev <ge.yanev@gmail.com>
2025-04-05 18:03:35 +0300

    Move the generation to a separate goroutine

--- cmd/urlshort-cyberark/main.go
+++ cmd/urlshort-cyberark/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"math/rand/v2"
 	"net/http"
 	"strings"
 )
+
+type LocalRequest struct {
+	Url     string
+	ReplyCh chan LocalResponse
+}
+
+type LocalResponse struct {
+	Url  string
+	Seed string
+	Err  error
+}
 
 type Request struct {
 	OriginalUrl string `json:"url"`
@@ -23,8 +36,33 @@ func main() {
 	allowedCh := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	allowedChSlice := strings.Split(allowedCh, "")
 	cache := make(map[string]string)
+	seedCh := make(chan LocalRequest, 100)
 
-	// database
+	go func(length int) {
+		err := errors.New("Unable to generate random seed")
+		var lResponse LocalResponse
+		// get remote seed
+		// generate some local seeds
+		for req := range seedCh {
+			var uFound bool
+			var rndChars string
+			for i := 0; i < 3; i++ {
+				rndChars = generateRandomUrlCharacters(length, allowedChSlice)
+				if _, ok := cache[rndChars]; !ok {
+					cache[rndChars] = req.Url
+					uFound = true
+					break
+				}
+			}
+			if !uFound {
+				lResponse.Err = err
+			}
+			lResponse.Seed = rndChars
+			lResponse.Url = req.Url
+			req.ReplyCh <- lResponse
+			close(req.ReplyCh)
+		}
+	}(5)
 
 	// HTTP server
 	postRequest := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -35,30 +73,28 @@ func main() {
 		}
 
 		// get the seed
-
-		// get our locally generated characters
-		rndChars := generateRandomUrlCharacters(3, allowedChSlice)
-		var uFound bool
-		for i := 0; i < 3; i++ {
-			if _, ok := cache[rndChars]; !ok {
-				cache[rndChars] = req.OriginalUrl
-				uFound = true
-				break
-			}
+		respCh := make(chan LocalResponse, 1)
+		localReq := LocalRequest{
+			Url:     req.OriginalUrl,
+			ReplyCh: respCh,
 		}
+		seedCh <- localReq
+		// wait
+		lRsp := <-respCh
+		fmt.Println(lRsp)
 
-		if !uFound {
-			w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Type", "application/json")
+		if lRsp.Err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		rsp := Response{
-			ShortCode:   rndChars
-			ShortUrl:    "http://short",
+			ShortCode:   lRsp.Seed,
+			ShortUrl:    "http://localhost:5000/" + lRsp.Seed,
 			OriginalUrl: req.OriginalUrl,
 		}
 
-		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(rsp)
 	})
 
